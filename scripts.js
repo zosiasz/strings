b4a5ed96afd2373ab28fa4e423baff593de5f9d7
zosia <zofiaszczech1@gmail.com>
2017-03-30 23:03:59 +0200

    123

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
 var animal = "Zielone słonie";
 var animalUpperCased = animal.toUpperCase();
-var textCharsAfter = text.replace('Papugi', animalUpperCased);
-console.log(textCharsAfter);
-console.log(textCharsAfter.length/2);
+	text = text.replace('Papugi', animalUpperCased);
+var partOfText = text.slice(0,text.length/2);
+console.log(partOfText);
+
